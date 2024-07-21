ff3ee52bfc0ea6607de6fbf995234fb33d22949f
Sridatta Nallamilli <sridattareddy13@gmail.com>
2024-07-21 17:07:20 +0530

    Adding initial package version

---
+++ DemoPackage/Sources/DemoSPM.xcframework/ios-arm64_x86_64-simulator/DemoCocoapod.framework/Headers/DemoCocoapod.h
@@ -0,0 +1,18 @@
+//
+//  DemoCocoapod.h
+//  DemoCocoapod
+//
+//  Created by Sridatta Nallamilli on 19/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoCocoapod.
+FOUNDATION_EXPORT double DemoCocoapodVersionNumber;
+
+//! Project version string for DemoCocoapod.
+FOUNDATION_EXPORT const unsigned char DemoCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoCocoapod/PublicHeader.h>
+
+
